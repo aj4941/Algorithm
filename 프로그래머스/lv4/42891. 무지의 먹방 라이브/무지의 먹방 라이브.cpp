aa3d3d8814b53bd8a20b40aa1973a47c7406a8a3
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-13 09:58:47 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 29.94 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.cpp
+++ 프로그래머스/lv4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.cpp
@@ -5,7 +5,7 @@ typedef pair<int, int> pii;
 
 int solution(vector<int> f, ll k) 
 {
-    vector<pii> v; k++;
+    vector<pii> v;
     for (int i = 0; i < f.size(); i++)
         v.push_back({ f[i], i + 1 });
     
@@ -14,15 +14,16 @@ int solution(vector<int> f, ll k)
     
     for (int i = 0; i < v.size(); i++)
     {
+        // k : 0-index
         ll d = v[i].first - cur;
         ll mv = d * len;
-        if (k <= mv)
+        if (k < mv)
         {
             vector<int> res;
             for (int j = i; j < v.size(); j++)
                 res.push_back(v[j].second);
             sort(res.begin(), res.end());
-            return res[(k - 1) % len];
+            return res[k % len];
         }
         else
         {
