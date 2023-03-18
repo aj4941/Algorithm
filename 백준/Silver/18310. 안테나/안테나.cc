c0b070b01aa5f59218729704838ab1a8d27b7f21
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-18 20:59:33 +0900

    [Silver III] Title: 안테나, Time: 36 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef pair<double, double> pdd;
+typedef vector<vector<ll>> vll;
+typedef tuple<int, int, int> ti;
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int n; cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	sort(a.begin(), a.end());
+
+	cout << a[(n - 1) / 2];
+}
