aaea4d7b90e2dbc8fbeb33e0afea79769f23565e
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-27 14:58:03 +0900

    [Unrated] Title: 민균이의 계략, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Unrated/11568. 민균이의 계략/민균이의 계략.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef pair<double, double> pdd;
+typedef vector<vector<ll>> vll;
+typedef tuple<ll, ll, ll> tl;
+typedef tuple<int, int, int> ti;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int n; cin >> n;
+	vector<int> a(n), v;
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	for (int i = 0; i < n; i++)
+	{
+		int idx = lower_bound(v.begin(), v.end(), a[i]) - v.begin();
+		if (idx == v.size())
+			v.push_back(a[i]);
+		else
+			v[idx] = a[i];
+	}
+
+	cout << v.size();
+}
