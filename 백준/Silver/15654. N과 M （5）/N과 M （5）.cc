cf1f3840609e08799ac31b58feccb385dff54247
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-05-02 12:17:31 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef pair<double, double> pdd;
+typedef vector<vector<ll>> vll;
+typedef tuple <ll, ll, ll, ll, ll> tl;
+typedef tuple<int, int, int> ti;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+int n, m;
+vector<int> a, stk;
+int cache[10];
+
+void dfs(int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << stk[i] << ' ';
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (cache[i] == 0) 
+		{
+			cache[i] = 1;
+			stk.push_back(a[i]);
+			dfs(cnt + 1);
+			stk.pop_back();
+			cache[i] = 0;
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		int x; cin >> x;
+		a.push_back(x);
+	}
+
+	sort(a.begin(), a.end());
+
+	dfs(0);
+}
