801a4f98ae3c8d981495df2efd11101a992662d3
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-05-12 20:58:35 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int ans_x = -1, ans_y = -1;
+
+void calc(int x, int y)
+{
+    if (ans_x == -1)
+    {
+        ans_x = x, ans_y = y;
+        return;
+    }
+    
+    if (y - x < ans_y - ans_x)
+        ans_x = x, ans_y = y;
+}
+
+vector<int> solution(vector<int> a, int k) 
+{
+    int n = a.size();
+    int l = 0, r = 0, sum = a[0];
+    while (true)
+    {
+        if (sum == k) calc(l, r);
+        
+        if (sum < k)
+        {
+            r++;
+            if (r == n) break;
+            sum += a[r];
+        }
+        else
+        {
+            sum -= a[l];
+            l++;
+            if (l > r) r = l;
+            if (r == n) break;
+        }
+        
+    }
+    
+    return { ans_x, ans_y };
+}
