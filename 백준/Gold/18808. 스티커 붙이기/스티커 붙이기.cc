fbdb32fe4ebed8c19904cf24f2971a1ed95a8efe
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-06-06 13:00:05 +0900

    [Gold III] Title: 스티커 붙이기, Time: 24 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
+++ 백준/Gold/18808. 스티커 붙이기/스티커 붙이기.cc
@@ -21,7 +21,8 @@ bool putSticker(int t, int x, int y, int r, int c)
 	bool flag = true;
 	for (int i = x; i < x + r; i++) for (int j = y; j < y + c; j++)
 	{
-		if (ans[i][j] && a[t][i - x][j - y])
+		// ans 보드에서는 (i, j) 이지만 스티커 입장에서는 무조건 (0, 0)에서 두는 것이므로 (i - x, j - y)로 탐색해야 한다.
+		if (ans[i][j] && a[t][i - x][j - y]) 
 		{
 			flag = false;
 			break;
@@ -47,14 +48,14 @@ void move(int t, int &r, int &c)
 	int b[20][20] = { 0 };
 	for (int i = 0; i < r; i++) for (int j = 0; j < c; j++)
 	{
-		b[j][r - 1 - i] = a[t][i][j];
-		a[t][i][j] = 0;
+		b[j][r - 1 - i] = a[t][i][j]; // (x, y) -> (y, n - 1 - x) 로 변하는 것을 관찰할 수 있다.
+		a[t][i][j] = 0; // 여기서 보드에 덧칠되는 스티커가 바뀌므로 0으로 리셋이 필요하다.
 	}
 
-	swap(r, c);
+	swap(r, c); // 90도 회전하면 r * c -> c * r로 바뀌는 것에 주의하자.
 
 	for (int i = 0; i < r; i++) for (int j = 0; j < c; j++)
-		a[t][i][j] = b[i][j];
+		a[t][i][j] = b[i][j]; // 90도 회전된 결과를 그대로 a에 복사한다.
 }
 
 int main(void)
@@ -75,13 +76,13 @@ int main(void)
 	{
 		// print();
 		int r = p[t].first, c = p[t].second;
-		for (int mv = 0; mv < 4; mv++) 
+		for (int mv = 0; mv < 4; mv++) // 0, 1, 2, 3 이 회전수이므로 mv < 3이 아니라 mv < 4로 둬야 함
 		{
 			// printSticker(t);
 			bool hasAns = false;
 			for (int x = 0; x < n && !hasAns; x++) for (int y = 0; y < m && !hasAns; y++)
 			{
-				if (x + r - 1 >= n || y + c - 1 >= m)
+				if (x + r - 1 >= n || y + c - 1 >= m) // 범위 초과 체크는 x + r이 아니라 x + r - 1이다. (x를 길이 1로 보므로)
 					continue;
 
 				if (putSticker(t, x, y, r, c))
