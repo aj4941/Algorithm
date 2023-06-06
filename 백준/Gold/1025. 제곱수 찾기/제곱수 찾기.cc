70c7fb260ce72eb5fe1827082bd913cd0fc6b42b
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-06-06 12:57:02 +0900

    [Gold V] Title: 제곱수 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.cc
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.cc
@@ -12,38 +12,44 @@ int dx[4] = { -1, 0, 1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
 const int N = 20;
-string s[N];
 int n, m;
-map<ll, ll> cache;
+string s[N];
 ll ans = -1;
+
+void calc(ll val)
+{
+	ll l = 0, r = 1e9; // [l, r)
+	while (l + 1 < r)
+	{
+		ll mid = (l + r) / 2;
+		if (mid * mid > val) r = mid;
+		else l = mid;
+	}
+
+	if (l * l == val)
+		ans = max(ans, val);
+}
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
-	for (ll num = 0; num <= 1e5; num++) 
-		cache[num * num] = 1;
-
 	cin >> n >> m;
-
 	for (int i = 0; i < n; i++) cin >> s[i];
 
-	for (int x = 0; x < n; x++) for (int y = 0; y < m; y++)
+	for (int xd = -n; xd <= n; xd++) for (int yd = -m; yd <= m; yd++)
 	{
-		ll val = s[x][y] - '0';
-		if (cache.count(val)) ans = max(ans, val);
-		if (val == 0) continue;
-		for (int xk = -n; xk < n; xk++) for (int yk = -m; yk < m; yk++)
+		if (xd == 0 && yd == 0) continue;
+		for (int x = 0; x < n; x++) for (int y = 0; y < m; y++)
 		{
-			string tmp = string("") + s[x][y];
-			if (xk == 0 && yk == 0) continue;
-			int nx = x + xk, ny = y + yk;
-			while (nx >= 0 && nx < n && ny >= 0 && ny < m)
+			int cx = x, cy = y;
+			ll val = s[cx][cy] - '0';
+			while (true)
 			{
-				tmp += s[nx][ny];
-				ll val = stoll(tmp);
-				if (cache.count(val)) ans = max(ans, val);
-				nx += xk, ny += yk;
+				calc(val);
+				cx += xd, cy += yd;
+				if (cx < 0 || cy < 0 || cx >= n || cy >= m) break;
+				val = val * 10 + s[cx][cy] - '0'; 
 			}
 		}
 	}
