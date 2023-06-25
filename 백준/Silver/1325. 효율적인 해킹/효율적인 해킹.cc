7bcba02c6a91c13a385f19d85f9c9c01d077e068
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-06-25 23:08:03 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2512 ms, Memory: 3232 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -3,49 +3,57 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef pair<double, double> pdd;
+typedef vector<vector<ll>> vll;
+typedef tuple <ll, ll, ll, ll, ll> tl;
+typedef tuple<int, int, int> ti;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-int mx = 0, res = 0;
-int cache[10002];
-vector<vector<int>> g;
-
-void dfs(int v)
-{
-	res++; 
-	cache[v] = 1;
-	for (int nv : g[v])
-	{
-		if (!cache[nv])
-			dfs(nv);
-	}
-}
+const int N = 10002;
+vector<int> g[N];
+bool cache[N];
+int res[N];
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	int n, m; cin >> n >> m;
-	g.resize(n + 1);
-	for (int i = 0; i < m; i++)
-	{
-		int u, v; cin >> u >> v;
-		g[v].push_back(u);
-	}
-	vector<int> tmp;
-	for (int i = 1; i <= n; i++)
-	{
-		memset(cache, 0, sizeof cache);
-		res = 0;
-		dfs(i);
-		if (mx < res)
-		{
-			tmp.clear();
-			tmp.push_back(i);
-			mx = res;
-		}
-		else if (mx == res)
-			tmp.push_back(i);
-	}
-	for (int to : tmp) cout << to << ' ';
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int u, v; cin >> u >> v;
+        g[v].push_back(u);
+    }
+
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        memset(cache, false, sizeof cache);
+        cache[i] = true;
+        int cnt = 1;
+
+        queue<int> q; q.push(i);
+        while (!q.empty())
+        {
+            int v = q.front(); q.pop();
+            for (int nv : g[v])
+            {
+                if (cache[nv]) continue;
+                cache[nv] = true;
+                q.push(nv); cnt++;
+            }
+        }
+
+        ans = max(ans, cnt);
+        res[i] = cnt;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (res[i] == ans)
+            cout << i << ' ';
+    }
 }
