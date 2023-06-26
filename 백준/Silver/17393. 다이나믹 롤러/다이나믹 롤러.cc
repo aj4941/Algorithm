50d3d1aa5637b655127fb4cbd99640ab2807240d
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-06-26 10:02:02 +0900

    [Silver IV] Title: 다이나믹 롤러, Time: 344 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/17393. 다이나믹 롤러/다이나믹 롤러.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef pair<double, double> pdd;
+typedef vector<vector<ll>> vll;
+typedef tuple <ll, ll, ll, ll, ll> tl;
+typedef tuple<int, int, int> ti;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int n; cin >> n;
+    vector<ll> a(n), b(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    for (int i = 0; i < n; i++)
+    {
+        int idx = upper_bound(b.begin() + i, b.end(), a[i]) - b.begin();
+        cout << idx - i - 1 << ' ';
+    }
+}
