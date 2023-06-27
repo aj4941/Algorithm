b7fd80c15e563dc3f24154688db66101f40f836d
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-06-27 17:21:35 +0900

    [Gold III] Title: 공주님의 정원, Time: 24 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -11,73 +11,66 @@ typedef tuple<int, int, int> ti;
 int dx[4] = { -1, 0, 1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
-int day[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-pii a[20][40];
+int month[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+int n;
+pii p[20][40];
 
 pii calc(pii a, pii b)
 {
-	if (a.first > b.first)
-		return a;
-	else if (a.first < b.first)
-		return b;
-	else
-	{
-		if (a.second > b.second)
-			return a;
-		else
-			return b;
-	}
+    if (a.first > b.first) return a;
+    else if (b.first > a.first) return b;
+    else if (a.second > b.second) return a;
+    else return b;
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	int n; cin >> n;
-	vector<pair<pii, pii>> v;
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    for (int i = 1; i <= 12; i++) for (int j = 1; j <= 31; j++) p[i][j] = pii(0, 0);
 
-	for (int i = 1; i <= 12; i++) for (int j = 1; j <= 32; j++)
-		a[i][j] = pii(0, 0);
+    cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		int sm, sd, em, ed; cin >> sm >> sd >> em >> ed;
-		pii res = calc(a[sm][sd], pii(em, ed));
-		a[sm][sd] = res;
-	}
+    for (int i = 0; i < n; i++)
+    {
+        int sm, sd, em, ed; cin >> sm >> sd >> em >> ed;
+        p[sm][sd] = calc(p[sm][sd], pii(em, ed));
+    }
 
-	int m = 1, d = 1;
+    int m = 1, d = 1;
 
-	while (m < 13)
-	{
-		int nd = d + 1, nm = m;
-		if (day[m] < nd)
-		{
-			nm++;
-			nd = 1;
-		}
+    while (true)
+    {
+        int nm = m, nd = d + 1;
+        if (nd > month[nm])
+        {
+            nm = m + 1;
+            nd = 1;
+        }
+        if (nm == 13) break;
+        p[nm][nd] = calc(p[m][d], p[nm][nd]);
+        m = nm, d = nd;
+    }
 
-		pii res = calc(a[nm][nd], a[m][d]);
-		a[nm][nd] = res;
-		m = nm, d = nd;
-	}
+    m = 3, d = 1;
+    int ans = 0;
+    bool hasAns = true;
 
-	ll ans = 0;
-	m = 3, d = 1;
+    while (true)
+    {
+        ans++;
+        pii nxt = p[m][d];
 
-	while (calc(pii(m, d), pii(11, 30)) == pii(11, 30))
-	{
-		pii res = a[m][d];
+        if (calc(pii(m, d), nxt) == pii(m, d))
+        {
+            hasAns = false;
+            break;
+        }
 
-		if (calc(res, pii(m, d)) == pii(m, d))
-		{
-			cout << 0;
-			return 0;
-		}
+        m = nxt.first, d = nxt.second;
+        if (m > 11) break;
+    }
 
-		ans++;
-		m = res.first, d = res.second;
-	}
-
-	cout << ans;
+    if (!hasAns) ans = 0;
+    cout << ans;
 }
