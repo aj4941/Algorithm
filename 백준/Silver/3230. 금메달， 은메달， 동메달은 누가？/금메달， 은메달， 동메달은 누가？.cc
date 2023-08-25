be38cfc9c3b0fb53fb7afd97a9306ebfa70dbf86
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-08-25 16:14:27 +0900

    [Silver V] Title: 금메달, 은메달, 동메달은 누가?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.cc
+++ 백준/Silver/3230. 금메달， 은메달， 동메달은 누가？/금메달， 은메달， 동메달은 누가？.cc
@@ -1,53 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 200;
-int r[N];
+typedef long long ll;
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     int n, m; cin >> n >> m;
-    vector<int> a(n), b(m);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < m; i++) cin >> b[i];
-
-    for (int i = 0; i < n; i++)
+    
+    vector<int> v;
+    for (int i = 1; i <= n; i++)
     {
-        int x = a[i];
-        int u = i + 1;
-        while (r[x] != 0)
-        {
-            int nu = r[x];
-            r[x] = u;
-            u = nu; x++;
-        }
-        r[x] = u;
+        int x; cin >> x;
+        v.insert(v.begin() + (x - 1), i);
     }
 
-    vector<int> vec;
-    for (int i = 1; i <= m; i++)
-        vec.push_back(r[i]);
-
-//    for (auto to : vec)
-//        cout << to << ' ';
-//    cout << endl;
-
-    memset(r, 0, sizeof r);
-
-    for (int i = 0; i < m; i++)
+    vector<int> v2;
+    for (int i = m - 1; i >= 0; i--)
     {
-        int x = b[i];
-        int u = vec[m - 1 - i];
-        while (r[x] != 0)
-        {
-            int nu = r[x];
-            r[x] = u;
-            u = nu; x++;
-        }
-        r[x] = u;
+        int x; cin >> x;
+        v2.insert(v2.begin() + (x - 1), v[i]);
     }
 
-    for (int i = 1; i <= 3; i++)
-        cout << r[i] << "\n";
+    for (int i = 0; i < 3; i++)
+        cout << v2[i] << "\n";
 }
