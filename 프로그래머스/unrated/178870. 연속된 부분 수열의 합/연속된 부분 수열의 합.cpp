257b79c2883b89f0e33c6a9bab0ba82d48092d43
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-08-25 20:04:55 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -1,41 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int ans_x = -1, ans_y = -1;
-
-void calc(int x, int y)
-{
-    if (ans_x == -1)
-    {
-        ans_x = x, ans_y = y;
-        return;
-    }
-    
-    if (y - x < ans_y - ans_x)
-        ans_x = x, ans_y = y;
-}
 
 vector<int> solution(vector<int> a, int k) 
 {
     int n = a.size();
     int l = 0, r = 0, sum = a[0];
+    int len = 1e9, ans_l = -1, ans_r = -1;
     while (true)
     {
-        if (sum == k) calc(l, r);
-        
-        if (sum < k)
+        if (sum == k)
+        {
+            if (r - l + 1 < len)
+            {
+                len = r - l + 1;
+                ans_l = l, ans_r = r;
+            }
+            sum -= a[l]; l++;
+        }
+        else if (sum > k)
+        {
+            sum -= a[l]; l++;
+            if (l == n) break;
+            if (l > r) r = l, sum = a[l];
+        }
+        else
         {
             r++;
             if (r == n) break;
             sum += a[r];
         }
-        else
-        {
-            sum -= a[l++];
-            if (l > r) r = l, sum = a[r];
-            if (r == n) break;
-        }
-        
     }
     
-    return { ans_x, ans_y };
+    return { ans_l, ans_r };
 }
