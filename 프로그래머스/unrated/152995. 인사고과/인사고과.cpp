fdcf01e6ec55fc276b3c8b1330091c91c18e7272
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-08-31 12:25:05 +0900

    [unrated] Title: 인사고과, Time: 44.07 ms, Memory: 27.3 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.cpp
@@ -1,38 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef pair<int, int> pii;
+int n;
 
-bool cmp(pair<int, int> &a, pair<int, int> &b)
+struct Node {
+    int x, y, idx;
+};
+
+bool cmp(Node &a, Node &b)
 {
-    if (a.first != b.first)
-        return a.first < b.first;
+    if (a.x != b.x)
+        return a.x < b.x;
     
-    return a.second > b.second;
+    return a.y < b.y;
 }
 
-int solution(vector<vector<int>> res)
+int solution(vector<vector<int>> v) 
 {
-    vector<pair<int, int>> v;
-    for (auto to : res)
-        v.push_back({ to[0], to[1] });
+    vector<Node> tmp;
+    multiset<int> ms;
+    n = v.size();
     
-    int a = v[0].first, b = v[0].second;
-    sort(v.begin(), v.end(), cmp);
-    
-    int n = v.size();
-    int ans = 1, mx = 0;
-    
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i < v.size(); i++)
     {
-        if (a < v[i].first && b < v[i].second)
-            return -1;
-        
-        if (mx <= v[i].second)
-        {
-            if (a + b < v[i].first + v[i].second)
-                ans++;
-            mx = max(mx, v[i].second);
-        }
+        int x = v[i][0], y = v[i][1];
+        tmp.push_back({ x, y, i });
+        ms.insert(y);
     }
     
-    return ans;
+    sort(tmp.begin(), tmp.end(), cmp);
+    int j = 0;
+    
+    vector<pii> res;
+    
+    for (int i = 0; i < n; i++)
+    {
+        while (j < n && tmp[i].x == tmp[j].x)
+        {
+            auto it = ms.find(tmp[j].y);
+            ms.erase(it);
+            j++;
+        }
+        
+        if (ms.size() == 0)
+        {
+            while (i < n)
+            {
+                res.push_back({ tmp[i].x + tmp[i].y, tmp[i].idx });
+                i++;
+            }
+            break;
+        }
+        
+        int mx = *prev(ms.end());
+        
+        if (tmp[i].y < mx)
+        {
+            if (tmp[i].idx == 0) 
+                return -1;
+            continue;
+        }
+        
+        res.push_back({ tmp[i].x + tmp[i].y, tmp[i].idx });
+    }
+    
+    sort(res.rbegin(), res.rend());
+    
+    int rank = 1;
+    
+    for (int i = 0; i < res.size(); i++)
+    {
+        // cout << i << ' ' << res[i].first << endl;
+        int idx = res[i].second;
+        if (i > 0) 
+        {
+            if (res[i - 1].first != res[i].first)
+                rank = i + 1;
+        }
+        
+        if (idx == 0)
+            return rank;
+    }
 }
