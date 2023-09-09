676237c7a7a00ff4caaf40efabcb7a6c6cf9af1d
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-09-09 16:33:40 +0900

    [Silver II] Title: 제야의 종, Time: 76 ms, Memory: 2564 KB -BaekjoonHub

--- 백준/Silver/18248. 제야의 종/제야의 종.cc
+++ 백준/Silver/18248. 제야의 종/제야의 종.cc
@@ -1,69 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const int N = 1002;
-int a[N][N];
-int p[N][N], indegree[N];
-vector<int> g[N];
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     int n, m; cin >> n >> m;
-    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++)
-        cin >> a[i][j];
+    vector<vector<int>> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int x; cin >> x;
+            v[i].push_back(x);
+        }
+    }
 
-//    if (n == 1)
-//    {
-//        cout << "YES";
-//        return 0;
-//    }
+    sort(v.begin(), v.end());
+
+    bool hasAns = true;
 
     for (int j = 0; j < m; j++)
     {
-        for (int x = 0; x < n; x++) for (int y = x + 1; y < n; y++)
+        for (int i1 = 0; i1 < n; i1++)
         {
-            if (a[x][j] == 1 && a[y][j] == 0) // x < y
-                p[x][y] = 1;
-            if (a[x][j] == 0 && a[y][j] == 1) // y < x
-                p[y][x] = 1;
+            for (int i2 = i1 + 1; i2 < n; i2++)
+            {
+                if (v[i1][j] > v[i2][j])
+                    hasAns = false;
+            }
         }
     }
 
-    for (int x = 0; x < n; x++) for (int y = 0; y < n; y++)
-    {
-        if (p[x][y])
-        {
-            g[x].push_back(y);
-            indegree[y]++;
-        }
-    }
-
-    queue<int> q;
-    int cnt = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (indegree[i] == 0)
-            q.push(i);
-    }
-
-    while (!q.empty())
-    {
-        int v = q.front(); q.pop();
-        cnt++;
-        for (int nv : g[v])
-        {
-            indegree[nv]--;
-            if (indegree[nv] == 0)
-                q.push(nv);
-        }
-    }
-
-    // cout << "cnt : " << cnt << endl;
-
-    if (cnt == n)
-        cout << "YES";
-    else
-        cout << "NO";
+    if (hasAns) cout << "YES";
+    else cout << "NO";
 }
