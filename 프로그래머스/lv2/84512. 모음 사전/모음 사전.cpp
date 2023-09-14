7b9b49a90fcce038335752cf173eaf77f42ec7c5
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-09-14 15:33:17 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.cpp
@@ -1,39 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-string t = "AEIOU";
-char prv[10];
-vector<string> ans;
+string ans;
+char ch[5] = { 'A', 'E', 'I', 'O', 'U' };
+int cnt = 0, ans_cnt = 0;
 
-void calc(int idx)
+void dfs(string tmp)
 {
-    string res = "";
-    for (int i = 0; i <= idx; i++)
-        res += prv[i];
+    if (tmp == ans)
+    {
+        ans_cnt = cnt;
+        return;
+    }
     
-    // cout << res << endl;
-    ans.push_back(res);
-}
-
-void dfs(int idx)
-{
-    if (idx == 5) return;
+    if (tmp.size() == 5)
+        return;
+    
     for (int i = 0; i < 5; i++)
     {
-        prv[idx] = t[i];
-        calc(idx);
-        dfs(idx + 1);
-    }    
+        cnt++;
+        dfs(tmp + ch[i]);
+    }
 }
 
 int solution(string w) 
 {
-    dfs(0);
-    sort(ans.begin(), ans.end());
-    
-    for (int i = 0; i < ans.size(); i++)
-    {
-        // cout << ans[i] << ' ' << w << endl;
-        if (ans[i] == w)
-            return i + 1;
-    }
+    ans = w; dfs("");
+    return ans_cnt;
 }
