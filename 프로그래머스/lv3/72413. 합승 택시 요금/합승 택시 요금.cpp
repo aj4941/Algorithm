7d0f7950467014893458aef126e938ed22cf41cb
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2023-10-03 15:48:31 +0900

    [level 3] Title: 합승 택시 요금, Time: 1.02 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,42 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef vector<vector<int>> vi;
-typedef pair<int, int> pii;
-const int N = 500;
-vector<pii> g[N];
-int dist[N][N];
+#include <string>
+#include <vector>
 
-int solution(int n, int s, int a, int b, vi fares)
-{
-    for (int i = 1; i <= n; i++) for (int j = 1; j <= n; j++)
-    {
-        if (i != j)
-            dist[i][j] = 1e9;
-    }
-    
-    for (auto to : fares)
-    {
-        int u = to[0], v = to[1], w = to[2];
-        dist[u][v] = dist[v][u] = w;
-    }
-    
-    for (int k = 1; k <= n; k++)
-    {
-        for (int i = 1; i <= n; i++) for (int j = 1; j <= n; j++)
-        {
-            if (dist[i][k] == 1e9 || dist[k][j] == 1e9) continue;
-            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-        }
-    }
-    
-    int ans = 1e9;
-    
-    for (int i = 1; i <= n; i++)
-    {
-        if (dist[s][i] == 1e9 || dist[i][a] == 1e9 || dist[i][b] == 1e9) continue;
-        int res = dist[s][i] + dist[i][a] + dist[i][b];
-        ans = min(ans, res);
-    }
-    
-    return ans;
+using namespace std;
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    int answer = 0;
+    return answer;
 }
