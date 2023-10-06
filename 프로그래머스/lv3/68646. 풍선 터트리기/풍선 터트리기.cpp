2d4d89c159ff13fc5a1cae8469b59912a0b7af9b
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2023-10-06 11:26:14 +0900

    [level 3] Title: 풍선 터트리기, Time: 8.83 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.cpp
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> pii;
+typedef long long ll;
 
-int solution(vector<int> a) {
-    
-    vector<pii> v;
+int solution(vector<int> a) 
+{
     int n = a.size();
-    if (n <= 2) return n;
+    vector<int> rmin(n + 1); 
+    rmin[n] = 1e9 - 5;
+    int lmin = 1e9 - 5;
+    
+    for (int i = n - 1; i >= 0; i--)
+        rmin[i] = min(a[i], rmin[i + 1]);
+    
+    int ans = 0;
     
     for (int i = 0; i < n; i++)
-        v.push_back({ a[i], i });
-    
-    sort(v.begin(), v.end());
-
-    int l = min(v[0].second, v[1].second);
-    int r = max(v[0].second, v[1].second);
-    int ans = 2;
-    
-    for (int i = 2; i < n; i++)
     {
-        int idx = v[i].second;
-        if (l <= idx && idx <= r) continue; 
-        ans++; l = min(l, idx); r = max(r, idx);
+        if (lmin > a[i] || rmin[i + 1] > a[i])
+        {
+            ans++;
+            // cout << a[i] << ' ';
+        }    
+        
+        lmin = min(lmin, a[i]);
     }
     
     return ans;
