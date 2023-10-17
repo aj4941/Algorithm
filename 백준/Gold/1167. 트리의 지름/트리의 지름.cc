e2697c3b33fe995ba267a556a6fd3a74d1dfda7a
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2023-10-17 11:28:00 +0900

    [Gold II] Title: 트리의 지름, Time: 84 ms, Memory: 9668 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -1,42 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+vector<vector<pll>> graph(100002);
+bool visit[100002];
+ll length = 0, longest;
 
-vector <pair<int,int>> v[100001]; // 간선과 가중치 
-bool visit[100001]; // 방문 노드
-int longest; // 가장 긴 정점
-int len; // 트리의 지름
-
-void dfs(int s,int d) {
-	visit[s] = 1; 
-	for (int i = 0; i < v[s].size(); i++) {
-		int y = v[s][i].first;
-		if (!visit[y]) {
-			dfs(y,v[s][i].second + d);
-		}
-	}
-	if (len < d) { // 방문한 정점이 누적 길이가 지금까지의 길이보다 크다면
-		len = d; // 길이를 바꿔주고
-		longest = s; // 현재 정점을 가장 긴 정점으로 만든다.
-	}
+void dfs(int v, ll len)
+{
+    visit[v] = true;
+    for(auto to : graph[v])
+    {
+        if(!visit[to.first])
+            dfs(to.first, len + to.second);
+    }
+    if(length < len)
+    {
+        length = len;
+        longest = v;
+    }
 }
-
-int main() {
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		int a; cin >> a;
-		while (1) {
-			int b, c;
-			cin >> b;
-			if (b == -1) break;
-			cin >> c;
-			v[a].push_back({ b,c });
-		}
-	}
-	dfs(1, 0); // 1을 기준으로 잡아서 가장 긴 정점을 찾고
-	memset(visit, 0, sizeof(visit));
-	dfs(longest, 0); // 그 정점으로 다시 가장 긴 지름을 찾는다.
-	cout << len << "\n";
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll u, v, w; cin >> u;
+        while(true)
+        {
+            cin >> v;
+            if(v == -1) break;
+            cin >> w;
+            graph[u].push_back({ v, w });
+        }
+    }
+    dfs(1, 0);
+    memset(visit, false, sizeof visit);
+    dfs(longest, 0);
+    cout << length;
 }
