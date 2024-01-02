cb4846b165347420135fcf4232a3f1611ee84de7
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-01-02 13:26:00 +0900

    [Silver IV] Title: 학생 번호, Time: 4 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.cc
+++ 백준/Silver/1235. 학생 번호/학생 번호.cc
@@ -1,41 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(void) 
+typedef pair<int, int> pii;
+typedef long long ll;
+int dx[8] = { -1, 1, 0, 0, 1, 1, -1, -1 };
+int dy[8] = { 0, 0, -1, 1, -1, 1, -1, 1 };
+const int N = 2000;
+string s[N];
+int main(void)
 {
-	int n; cin >> n;
-	vector<string> s(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> s[i];
-		reverse(s[i].begin(), s[i].end());
-	}
-	
-	int len = s[0].size();
-	
-	while (len > 0)
-	{
-		bool flag = true;
-		unordered_map<string, bool> mp;
-		for (int i = 0; i < n; i++)
-		{
-			if (mp.count(s[i]))
-			{
-				flag = false;
-				break;
-			}
-			mp[s[i]] = true;
-		}
-		
-		if (!flag)
-			break;
-			
-		len--;
-		
-		for (int i = 0; i < n; i++)
-			s[i].pop_back();
-	}
-	
-	cout << len + 1;
-	return 0;
+    int n; cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+        reverse(s[i].begin(), s[i].end());
+    }
+
+    int l = -1, r = s[0].size() - 1;
+    while (l + 1 < r) // (l, r]
+    {
+        int mid = (l + r) / 2;
+        map<string, bool> mp;
+        bool flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            string tmp = "";
+            for (int j = 0; j <= mid; j++)
+                tmp += s[i][j];
+            if (mp.count(tmp))
+                flag = false;
+            mp[tmp] = true;
+        }
+
+        if (flag) r = mid;
+        else l = mid;
+    }
+
+    cout << r + 1;
 }
