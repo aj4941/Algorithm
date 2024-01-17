934c05f3cb0a3405eded7b289caeba224dd4bec9
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-01-17 09:46:21 +0900

    [Gold II] Title: 연료가 부족해, Time: 104 ms, Memory: 72680 KB -BaekjoonHub

--- 백준/Gold/20293. 연료가 부족해/연료가 부족해.cc
+++ 백준/Gold/20293. 연료가 부족해/연료가 부족해.cc
@@ -5,27 +5,17 @@ typedef long long ll;
 const ll inf = 1000000007;
 int dx[4] = { -1, 1, 0, 0 };
 int dy[4] = { 0, 0, -1, 1 };
+const int N = 3002;
 int n, m, t;
-int dp[2002];
-vector<pair<pii, int>> v;
+int dp[N][N], gas[N][N];
 
-int solve(int idx)
+int solve(int x, int y)
 {
-    int &ret = dp[idx];
+    int &ret = dp[x][y];
     if (ret != -1) return ret;
-    int x = v[idx].first.first, y = v[idx].first.second, f = v[idx].second;
-    ret = -1e9;
-
-    for (int i = 0; i < v.size(); i++)
-    {
-        if (i == idx) continue;
-        int nx = v[i].first.first, ny = v[i].first.second;
-        if (x < nx || y < ny) continue;
-        int dist = abs(x - nx) + abs(y - ny);
-        int dpValue = solve(i);
-        if (dpValue - dist >= 0)
-            ret = max(ret, dpValue - dist + f);
-    }
+    ret = 1e9;
+    if (x + 1 <= n) ret = min(ret, max(0, solve(x + 1, y) + 1 - gas[x][y]));
+    if (y + 1 <= m) ret = min(ret, max(0, solve(x, y + 1) + 1 - gas[x][y]));
 
     return ret;
 }
@@ -34,26 +24,14 @@ int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
+    memset(dp, -1, sizeof dp);
     cin >> n >> m >> t;
-    v.push_back({ { 1, 1 }, 0 });
+    dp[n][m] = 0;
     for (int i = 0; i < t; i++)
     {
-        int x, y, f; cin >> x >> y >> f;
-        v.push_back({ { x, y }, f });
-    }
-    v.push_back({ { n, m }, 0 });
-
-    int l = -1, r = 1e9;
-    while (l + 1 < r) // (l, r]
-    {
-        memset(dp, -1, sizeof dp);
-        int mid = (l + r) / 2;
-        dp[0] = mid;
-        int res = solve(v.size() - 1);
-
-        if (res == -1e9) l = mid;
-        else r = mid;
+        int x, y, w; cin >> x >> y >> w;
+        gas[x][y] = w;
     }
 
-    cout << r;
+    cout << solve(1, 1);
 }
