9d92524f637453ec827e49d97a0fe354d29c5fd7
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-01-27 11:13:22 +0900

    [Platinum V] Title: 광부 호석, Time: 76 ms, Memory: 15032 KB, Score: 16 point -BaekjoonHub

--- 백준/Platinum/21279. 광부 호석/광부 호석.cc
+++ 백준/Platinum/21279. 광부 호석/광부 호석.cc
@@ -6,43 +6,54 @@ typedef pair<ll, ll> pll;
 typedef unsigned long long ull;
 typedef pair<double, double> pdd;
 typedef vector<vector<ll>> vll;
-int dx[4] = { 0, 0, -1, 1 };
-int dy[4] = { -1, 1, 0, 0 };
+typedef tuple <ll, ll, ll, ll, ll> tl;
+typedef tuple<int, int, int> ti;
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
 ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
-const ll N = 100002;
+const int N = 200002;
 vector<pll> v[N];
-priority_queue<pll> pq;
 
 int main(void)
 {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-    ll n, C; cin >> n >> C;
-    for (ll i = 0; i < n; i++)
+    ll n, c; cin >> n >> c;
+
+    for (int i = 0; i < n; i++)
     {
-        ll x, y, k; cin >> x >> y >> k;
-        v[x].push_back({ y, k });
+        ll x, y, val; cin >> x >> y >> val;
+        v[x].push_back({ y, val });
     }
 
-    ll max_y = 1e5;
+    int y = 1e5;
+    priority_queue<pll> pq;
     ll ans = 0, res = 0;
-    
-    for (ll x = 0; x <= 1e5; x++) 
+
+    for (int x = 0; x <= 1e5; x++)
     {
-        if (v[x].size() == 0) continue;
-        for (pll to : v[x])
+        for (auto to : v[x])
         {
-            if (to.first <= max_y)
-            {
-                pq.push({ to.first, -to.second });
-                res += to.second;
-            }
+            ll yi = to.first, val = to.second;
+            if (y < yi) continue;
+            res += val; pq.push({ yi, -val });
+        }
+        while (pq.size() > c)
+        {
+            ll yi = pq.top().first, val = -pq.top().second; pq.pop();
+            y = yi; res -= val;
         }
 
-        while (pq.size() > C)
+        while (pq.size())
         {
-            res -= -pq.top().second; pq.pop();
-            max_y = pq.top().first;
+            ll yi = pq.top().first, val = -pq.top().second;
+            if (y <= yi)
+            {
+                res -= val;
+                pq.pop();
+            }
+            else
+                break;
         }
 
         ans = max(ans, res);
