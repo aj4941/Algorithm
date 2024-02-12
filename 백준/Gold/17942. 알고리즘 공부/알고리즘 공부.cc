53a99ceb5370cf3d5712052fcd6d2f12a6788eab
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-02-12 22:22:52 +0900

    [Gold I] Title: 알고리즘 공부, Time: 72 ms, Memory: 8536 KB -BaekjoonHub

--- 백준/Gold/17942. 알고리즘 공부/알고리즘 공부.cc
+++ 백준/Gold/17942. 알고리즘 공부/알고리즘 공부.cc
@@ -5,63 +5,45 @@ typedef vector<vector<int>> vi;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 const int N = 100002;
-vector<pll> g[N];
+int n, m, k;
+vector<pii> g[N];
+bool cache[N];
 
 int main(void)
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int n, m; cin >> n >> m;
-    vector<ll> a(n + 1);
+    cin >> n >> m;
+    vector<int> a(n + 1);
     for (int i = 1; i <= n; i++) cin >> a[i];
-    int k; cin >> k;
+    cin >> k;
     for (int i = 0; i < k; i++)
     {
         int u, v, w; cin >> u >> v >> w;
         g[u].push_back({ v, w });
     }
 
-    ll l = 0, r = 1e15;
+    priority_queue<pii> pq;
+    for (int i = 1; i <= n; i++)
+        pq.push({ -a[i], i });
 
-    while (l + 1 < r)
+    int ans = 0;
+
+    while (!pq.empty())
     {
-        ll mid = (l + r) / 2;
-        ll cnt = 0;
-        vector<bool> cache(n + 1, false);
-        vector<ll> sa = a;
-        queue<ll> q;
-
-        for (int i = 1; i <= n; i++)
+        auto to = pq.top(); pq.pop();
+        int val = -to.first, v = to.second;
+        if (cache[v]) continue;
+        ans = max(ans, val); cache[v] = true;
+        m--;
+        if (m == 0) break;
+        for (auto to : g[v])
         {
-            if (sa[i] <= mid)
-            {
-                q.push(i); cnt++;
-                cache[i] = true;
-            }
+            int nv = to.first, w = to.second;
+            a[nv] -= w;
+            pq.push({ -a[nv], nv });
         }
-
-        while (!q.empty())
-        {
-            ll v = q.front(); q.pop();
-            for (auto to : g[v])
-            {
-                ll nv = to.first, w = to.second;
-                sa[nv] -= w;
-                if (cache[nv]) continue;
-                if (sa[nv] <= mid)
-                {
-                    q.push(nv);
-                    cache[nv] = true;
-                    cnt++;
-                }
-            }
-        }
-
-       // cout << mid << ' ' << cnt << endl;
-
-        if (cnt >= m) r = mid;
-        else l = mid;
     }
 
-    cout << r;
+    cout << ans;
 }
