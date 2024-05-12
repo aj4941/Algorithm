1ecdbf89fb678ce2594991ad2dbc17394a0d0b7f
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-05-12 21:15:01 +0900

    [Gold II] Title: 경로 찾기, Time: 108 ms, Memory: 59164 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.cc
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.cc
@@ -5,38 +5,46 @@ typedef vector<vector<int>> vi;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<vector<ll>> vll;
-int dx[4] = { 1, 0, 0, 0 };
-int dy[4] = { 0, 1, -1, 1 };
+int dx[4] = { -1, 0, 0, 0 };
+int dy[4] = { 0, -1, -1, 1 };
+const int N = 52;
+ll dp[N][N][N][N];
 ll n, m, c;
-ll dp[52][52][52][52];
-int cache[52][52];
+ll cache[N][N];
 const ll inf = 1000007;
-int cnt = 0;
 
-ll solve(int x, int y, int c, int prv)
+ll solve(int x, int y, int c, int k)
 {
-    ll &ret = dp[x][y][c][prv];
+    ll &ret = dp[x][y][c][k];
     if (ret != -1) return ret;
     ret = 0;
-    if (x == n && y == m)
+    if (x == 1 && y == 1)
     {
-        if (c == cnt) return ret = 1;
+        if (k == 0) return ret = 1;
         else return ret = 0;
     }
     for (int i = 0; i < 2; i++)
     {
         int nx = x + dx[i], ny = y + dy[i];
-        if (nx > n || ny > m) continue;
-        int pos = cache[nx][ny];
-        if (pos == 0)
+        if (nx < 1 || ny < 1) continue;
+        if (cache[nx][ny] == 0)
         {
-            ret += solve(nx, ny, c, prv);
+            ret += solve(nx, ny, c, k);
             ret %= inf;
         }
-        else if (c + 1 <= cnt && prv < pos)
+        else
         {
-            ret += solve(nx, ny, c + 1, pos);
-            ret %= inf;
+            if (k == 0) continue;
+            if (c == 0)
+            {
+                ret += solve(nx, ny, cache[nx][ny], k - 1);
+                ret %= inf;
+            }
+            else if (cache[nx][ny] < c)
+            {
+                ret += solve(nx, ny, cache[nx][ny], k - 1);
+                ret %= inf;
+            }
         }
     }
     return ret;
@@ -46,19 +54,18 @@ int main(void)
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+    memset(dp, -1, sizeof dp);
     cin >> n >> m >> c;
     for (int i = 1; i <= c; i++)
     {
-        ll x, y; cin >> x >> y;
+        int x, y; cin >> x >> y;
         cache[x][y] = i;
     }
 
-    int count = cache[1][1] > 0 ? 1 : 0;
-    int prv = cache[1][1];
     for (int i = 0; i <= c; i++)
     {
-        memset(dp, -1, sizeof dp);
-        cout << solve(1, 1, count, prv) << " ";
-        cnt++;
+        int cnt = i - ((cache[n][m] > 0) ? 1 : 0);
+        if (cnt < 0) cout << 0 << ' ';
+        else cout << solve(n, m, cache[n][m], cnt) << ' ';
     }
 }
