55725e13330e8ff91d5fbdd6ffb112daa837d72b
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-16 16:44:51 +0900

    [Silver IV] Title: 물건 팔기, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1487. 물건 팔기/물건 팔기.cc
+++ 백준/Silver/1487. 물건 팔기/물건 팔기.cc
@@ -1,45 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef vector<vector<int>> vi;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef unsigned long long ull;
-typedef pair<double, double> pdd;
 typedef vector<vector<ll>> vll;
-typedef tuple <ll, ll, ll, ll, ll> tl;
-typedef tuple<int, int, int> ti;
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     int n; cin >> n;
-    vector<pii> a(n);
+    vector<pll> a(n);
     for (int i = 0; i < n; i++)
         cin >> a[i].first >> a[i].second;
 
-    int ans_M = 0, getM = 0;
-    
-    for (int M = 1; M <= 1e6; M++)
+    ll mx = 0, ans = 0;
+
+    for (ll val = 1e6; val >= 0; val--)
     {
-        int res = 0;
+        ll res = 0;
         for (int i = 0; i < n; i++)
         {
-            if (a[i].first < M) continue;
-            int val = M - a[i].second;
-            if (val < 0) continue;
-            res += val;
+            if (a[i].first < val) continue;
+            if (val - a[i].second < 0) continue;
+            res += val - a[i].second;
         }
 
-        if (res > getM)
+        if (mx <= res)
         {
-            getM = res;
-            ans_M = M;
+            mx = res;
+            ans = val;
         }
     }
 
-    cout << ans_M;
+    cout << ans;
 }
