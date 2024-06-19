6773947826bd531fc21c2f3874b90fbc9e644b77
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-19 14:41:55 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,50 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef vector<vector<int>> vi;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef unsigned long long ull;
-typedef pair<double, double> pdd;
 typedef vector<vector<ll>> vll;
-typedef tuple <ll, ll, ll, ll, ll> tl;
-typedef tuple<int, int, int> ti;
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
 int n, m;
-vector<int> a, stk;
-int cache[10];
+int a[10];
+bool cache[10];
+vector<int> res;
 
-void dfs(int cnt)
+void func(int cnt)
 {
-	if (cnt == m)
-	{
-		for (int i = 0; i < m; i++)
-			cout << stk[i] << ' ';
-		cout << "\n";
-		return;
-	}
+    if (cnt == m)
+    {
+        for (auto to : res)
+            cout << to << ' ';
+        cout << "\n";
+        return;
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		if (cache[a[i]] == 0) 
-		{
-			cache[a[i]] = 1;
-			stk.push_back(a[i]);
-			dfs(cnt + 1);
-			stk.pop_back();
-			cache[a[i]] = 0;
-		}
-	}
+    for (int i = 1; i <= n; i++)
+    {
+        if (cache[i] == false)
+        {
+            cache[i] = true;
+            res.push_back(i);
+            func(cnt + 1);
+            res.pop_back();
+            cache[i] = false;
+        }
+    }
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		a.push_back(i + 1);
-
-	dfs(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    func(0);
 }
