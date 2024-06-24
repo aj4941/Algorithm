723f3b2f396fcf086466cbf707db1b21746d754a
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-24 14:52:32 +0900

    [Silver I] Title: 출근, Time: 112 ms, Memory: 33672 KB -BaekjoonHub

--- 백준/Silver/13903. 출근/출근.cc
+++ 백준/Silver/13903. 출근/출근.cc
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> pii;
 typedef long long ll;
+typedef vector<vector<int>> vi;
+typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-int dx[4] = { -1, 1, 0, 0 };
-int dy[4] = { 0, 0, -1, 1 };
-const int N = 1020;
+typedef vector<vector<ll>> vll;
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+ll gcd(ll x, ll y) {
+    return ((y) ? gcd(y, x % y) : x);
+}
+
+const int N = 2000;
 int a[N][N], dist[N][N];
-int n, m, k;
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    cin >> n >> m;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, m; cin >> n >> m;
     for (int i = 0; i < n; i++) for (int j = 0; j < m; j++)
         cin >> a[i][j];
 
-    cin >> k;
-    vector<pii> tmp;
+    int k; cin >> k;
+    vector<pii> pos;
     for (int i = 0; i < k; i++)
     {
         int x, y; cin >> x >> y;
-        tmp.push_back({ x, y });
+        pos.push_back({ x, y });
     }
 
-    memset(dist, -1, sizeof dist);
     queue<pii> q;
+    memset(dist, -1, sizeof dist);
 
     for (int j = 0; j < m; j++)
     {
@@ -41,11 +46,12 @@ int main(void)
     {
         auto to = q.front(); q.pop();
         int x = to.first, y = to.second;
-        for (int i = 0; i < tmp.size(); i++)
+
+        for (auto to : pos)
         {
-            int nx = x + tmp[i].first, ny = y + tmp[i].second;
+            int nx = x + to.first, ny = y + to.second;
             if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            if (dist[nx][ny] != -1 || a[nx][ny] == 0) continue;
+            if (a[nx][ny] == 0 || dist[nx][ny] != -1) continue;
             dist[nx][ny] = dist[x][y] + 1;
             q.push({ nx, ny });
         }
@@ -54,10 +60,11 @@ int main(void)
     int ans = 1e9;
     for (int j = 0; j < m; j++)
     {
-        if (dist[n - 1][j] != -1)
-            ans = min(ans, dist[n - 1][j]);
+        if (dist[n - 1][j] == -1) continue;
+        ans = min(ans, dist[n - 1][j]);
     }
 
     if (ans == 1e9) ans = -1;
+    
     cout << ans;
 }
