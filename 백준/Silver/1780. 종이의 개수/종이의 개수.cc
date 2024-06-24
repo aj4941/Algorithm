a4a1d9138c09f7c6f2ade438d46ec903fd0b17d9
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-24 12:06:30 +0900

    [Silver II] Title: 종이의 개수, Time: 408 ms, Memory: 21568 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,48 +1,55 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int Point[4] = { 0, 0, 0, 0 };
-int Paper[3000][3000] = { 0 };
-void DivideConquer(int N, int r, int c)
-{
-	int i, j;
-	int num = Paper[r][c];
-	if(N == 1)
-	{
-		Point[num + 1]++;
-		return;
-	}
-	for(i = r; i < r + N; i++) for(j = c; j < c + N; j++)
-	{
-		if(num != Paper[i][j])
-		{
-			DivideConquer(N / 3, r, c);
-			DivideConquer(N / 3, r + N / 3, c);
-			DivideConquer(N / 3, r + (N / 3) * 2, c);
-			DivideConquer(N / 3, r, c + N / 3);
-			DivideConquer(N / 3, r + N / 3, c + N / 3);
-			DivideConquer(N / 3, r + (N / 3) * 2, c + N / 3);
-			DivideConquer(N / 3, r, c + (N / 3) * 2);
-			DivideConquer(N / 3, r + (N / 3), c + (N / 3) * 2);
-			DivideConquer(N / 3, r + (N / 3) * 2, c + (N / 3) * 2);
-			return;
-		}
-	}
-	if(i == r + N && j == c + N)
-	{
-		Point[num + 1]++;
-		return;
-	}
-	
+typedef long long ll;
+typedef vector<vector<int>> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<vector<ll>> vll;
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+ll gcd(ll x, ll y) {
+    return ((y) ? gcd(y, x % y) : x);
 }
+const int N = 27 * 27 * 3 + 50;
+int n, a[N][N];
+int r1, r2, r3;
+
+void solve(int x1, int y1, int x2, int y2)
+{
+    bool flag = true;
+    for (int i = x1; i < x2; i++) for (int j = y1; j < y2; j++)
+    {
+        if (a[x1][y1] != a[i][j])
+            flag = false;
+    }
+
+    if (flag)
+    {
+        if (a[x1][y1] == -1) r1++;
+        if (a[x1][y1] == 0) r2++;
+        if (a[x1][y1] == 1) r3++;
+        return;
+    }
+
+    int sz = (x2 - x1) / 3;
+
+    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++)
+    {
+        int cx = x1 + sz * i, cy = y1 + sz * j;
+        int ncx = cx + sz, ncy = cy + sz;
+        solve(cx, cy, ncx, ncy);
+    }
+}
+
 int main(void)
 {
-	int N, i, j;
-	cin >> N;
-	for(i = 0; i < N; i++) for(j = 0; j < N; j++)
-		scanf("%d", &Paper[i][j]);
-		
-	DivideConquer(N, 0, 0);
-	for(i = 0; i <= 2; i++)
-		printf("%d\n", Point[i]);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++)
+        cin >> a[i][j];
+
+    solve(0, 0, n, n);
+
+    cout << r1 << '\n' << r2 << '\n' << r3;
 }
