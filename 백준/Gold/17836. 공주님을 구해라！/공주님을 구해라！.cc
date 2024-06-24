d4f041834e86a3bca1e9aa2b483d30863861f1e5
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-24 15:50:22 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 0 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.cc
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.cc
@@ -1,65 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef vector<vector<int>> vi;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef unsigned long long ull;
-typedef pair<double, double> pdd;
 typedef vector<vector<ll>> vll;
-typedef tuple <ll, ll, ll> tl;
-typedef tuple<int, int, int> ti;
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-ll gcd(ll a, ll b) { for (; b; a %= b, swap(a, b)); return a; }
-const ll N = 200;
-ll a[N][N];
-ll dist[N][N][2];
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+
+ll gcd(ll x, ll y) {
+    return ((y) ? gcd(y, x % y) : x);
+}
+
+const int N = 200;
+int dist[N][N][2], a[N][N];
+int n, m, t;
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	memset(dist, -1, sizeof dist);
-	ll n, m, t; cin >> n >> m >> t;
-	for (ll i = 0; i < n; i++) for (ll j = 0; j < m; j++)
-		cin >> a[i][j];
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    memset(dist, -1, sizeof dist);
+    cin >> n >> m >> t;
 
-	dist[0][0][0] = 0;
-	queue<tl> q; q.push({ 0, 0, 0 });
+    for (int i = 0; i < n; i++) for (int j = 0; j < m; j++)
+        cin >> a[i][j];
 
-	while (!q.empty())
-	{
-		auto to = q.front(); q.pop();
-		ll x = get<0>(to), y = get<1>(to), k = get<2>(to);
-		ll d = dist[x][y][k];
-		for (ll i = 0; i < 4; i++)
-		{
-			ll nx = x + dx[i], ny = y + dy[i];
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-			if (dist[nx][ny][k] != -1) continue;
-			if (a[nx][ny] == 2)
-			{
-				dist[nx][ny][1] = d + 1;
-				q.push({ nx, ny, 1 });
-			}
-			else if (a[nx][ny] == 1)
-			{
-				if (k == 0) continue;
-				dist[nx][ny][k] = d + 1;
-				q.push({ nx, ny, k });
-			}
-			else
-			{
-				dist[nx][ny][k] = d + 1;
-				q.push({ nx, ny, k });
-			}
-		}
-	}
+    memset(dist, -1, sizeof dist);
+    queue<tuple<int, int, int>> q; q.push({ 0, 0, 0 });
+    dist[0][0][0] = 0;
 
-	ll ans = 1e9;
-	if (dist[n - 1][m - 1][0] != -1) ans = dist[n - 1][m - 1][0];
-	if (dist[n - 1][m - 1][1] != -1) ans = min(ans, dist[n - 1][m - 1][1]);
+    while (!q.empty())
+    {
+        auto to = q.front(); q.pop();
+        int x = get<0>(to), y = get<1>(to), c = get<2>(to);
 
-	if (ans == 1e9 || ans > t) cout << "Fail";
-	else cout << ans;
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i], ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if (dist[nx][ny][c] != -1) continue;
+            if (c == 0 && a[nx][ny] == 1) continue;
+            int nc = (a[nx][ny] == 2) ? 1 : c;
+
+            dist[nx][ny][nc] = dist[x][y][c] + 1;
+            q.push({ nx, ny, nc });
+        }
+    }
+
+    int r1 = (dist[n - 1][m - 1][0] == -1) ? 1e9 : dist[n - 1][m - 1][0];
+    int r2 = (dist[n - 1][m - 1][1] == -1) ? 1e9 : dist[n - 1][m - 1][1];
+
+    int ans = min(r1, r2);
+
+    if (ans > t) cout << "Fail";
+    else cout << ans;
 }
