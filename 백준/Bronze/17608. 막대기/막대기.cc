013ac1fa13420524849a46b1ac7124322e9a5b30
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-06-24 11:43:39 +0900

    [Bronze II] Title: 막대기, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.cc
+++ 백준/Bronze/17608. 막대기/막대기.cc
@@ -1,21 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef vector<vector<int>> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<vector<ll>> vll;
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
+ll gcd(ll x, ll y) {
+    return ((y) ? gcd(y, x % y) : x);
+}
 
-int main(void) {
-  int n; cin >> n;
-  vector<int> a(n);
-  for (int i = 0; i < n; i++) cin >> a[i];
-  int mx = a[n - 1], cnt = 1;
-  for (int i = n - 2; i >= 0; i--)
-  {
-    if (a[i] > mx)
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    reverse(a.begin(), a.end());
+
+    int ans = 1, mx = a[0];
+
+    for (int i = 1; i < n; i++)
     {
-      cnt++;
-      mx = a[i];
+        if (mx < a[i])
+            ans++, mx = a[i];
     }
-  }
 
-  cout << cnt;
-  
-  return 0;
+    cout << ans;
 }
