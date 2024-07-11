2feb3597072db98e4cfd6651034e6dbeb05f5084
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-07-11 22:27:12 +0900

    [Silver II] Title: 최대 페이지 수, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.cc
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.cc
@@ -7,33 +7,35 @@ typedef pair<ll, ll> pll;
 typedef vector<vector<ll>> vll;
 int dx[4] = { 1, 0, 0, 0 };
 int dy[4] = { 0, 1, 1, -1 };
+int n, m;
+ll dp[22][202];
+vector<pll> a;
+
+ll solve(int idx, int days)
+{
+    ll &ret = dp[idx][days];
+    if (ret != -1) return ret;
+    if (idx == m) return ret = 0;
+    ret = solve(idx + 1, days); // 선택하지 않는 경우
+
+    if (days + a[idx].first <= n)
+        ret = max(ret, solve(idx + 1, days + a[idx].first) + a[idx].second);
+
+    return ret;
+}
 
 int main(void)
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int n, m; cin >> n >> m;
-    vector<pii> a(m);
+    cin >> n >> m;
     for (int i = 0; i < m; i++)
-        cin >> a[i].first >> a[i].second;
-
-    int ans = 0;
-
-    for (int bit = 1; bit < (1 << m); bit++)
     {
-        int days = 0, page = 0;
-        for (int i = 0; i < m; i++)
-        {
-            if (bit & (1 << i))
-            {
-                days += a[i].first;
-                page += a[i].second;
-            }
-        }
-
-        if (days <= n)
-            ans = max(ans, page);
+        ll d, p; cin >> d >> p;
+        a.push_back({ d, p });
     }
 
-    cout << ans;
+    memset(dp, -1, sizeof dp);
+
+    cout << solve(0, 0);
 }
