d9b64ca427b79ed94f55ee6603996dd5be1a1c75
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-08-27 10:35:06 +0900

    [Gold I] Title: 배열 정렬, Time: 48 ms, Memory: 3896 KB -BaekjoonHub

--- 백준/Gold/28707. 배열 정렬/배열 정렬.cc
+++ 백준/Gold/28707. 배열 정렬/배열 정렬.cc
@@ -6,8 +6,31 @@ typedef pair<int, int> pii;
 int dx[4] = { -1, 1, 0, 0 };
 int dy[4] = { 0, 0, -1, 1 };
 const int N = 10;
-map<vector<int>, int> dist;
+unordered_map<int, int> dist;
 int n, m;
+
+int cal_num(vector<int>& a)
+{
+    int val = 0, mul = 1;
+    for (int i = (int)a.size() - 1; i >= 0; i--)
+    {
+        val += a[i] * mul;
+        mul *= 10;
+    }
+    return val;
+}
+
+vector<int> cal_vec(int val)
+{
+    vector<int> a;
+    while (a.size() < n)
+    {
+        a.push_back(val % 10);
+        val /= 10;
+    }
+    reverse(a.begin(), a.end());
+    return a;
+}
 
 int main(void)
 {
@@ -15,9 +38,11 @@ int main(void)
     cin.tie(0); cout.tie(0);
     cin >> n;
     vector<int> a(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    dist[a] = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        a[i]--;
+    }
     cin >> m;
     vector<pair<pii, int>> p;
     for (int i = 0; i < m; i++)
@@ -26,31 +51,34 @@ int main(void)
         p.push_back({ { l - 1, r - 1 }, c });
     }
 
-    priority_queue<pair<int, vector<int>>> pq;
-    pq.push({ 0, a });
+    priority_queue<pair<int, int>> pq;
+    pq.push({ 0, cal_num(a) });
 
     while (!pq.empty())
     {
         auto to = pq.top(); pq.pop();
         int d = -to.first;
-        vector<int> cur = to.second;
-        if (dist[cur] != d) continue;
+        int num = to.second;
+        if (dist[num] != d) continue;
+        vector<int> cur = cal_vec(num);
         for (int i = 0; i < m; i++)
         {
             int c = p[i].second;
             int l = p[i].first.first, r = p[i].first.second;
             swap(cur[l], cur[r]);
-            if (!dist.count(cur) || d + c < dist[cur])
+            int nnum = cal_num(cur);
+            if (!dist.count(nnum) || d + c < dist[nnum])
             {
-                dist[cur] = d + c;
-                pq.push({ -dist[cur], cur });
+                dist[nnum] = d + c;
+                pq.push({ -dist[nnum], nnum });
             }
             swap(cur[l], cur[r]);
         }
     }
 
     sort(a.begin(), a.end());
+    int ans = cal_num(a);
 
-    if (!dist.count(a)) cout << -1;
-    else cout << dist[a];
+    if (!dist.count(ans)) cout << -1;
+    else cout << dist[ans];
 }
