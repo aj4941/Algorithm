ae2b34198ea2fb074d60f01acd3675e78d6ac23b
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-09-12 23:33:47 +0900

    [Silver II] Title: 내 뒤에 나와 다른 수, Time: 200 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Silver/24523. 내 뒤에 나와 다른 수/내 뒤에 나와 다른 수.cc
+++ 백준/Silver/24523. 내 뒤에 나와 다른 수/내 뒤에 나와 다른 수.cc
@@ -5,52 +5,27 @@ typedef pair<ll, ll> pll;
 typedef pair<int, int> pii;
 int dx[4] = { 0, 1, 0, 0 };
 int dy[4] = { 1, 0, -1, 1 };
+const int N = 1000002;
+int dp[N];
 
 int main(void)
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int n; cin >> n;
-    vector<int> a(n + 1);
+    vector<int> a(n + 1), ans(n + 1);
     for (int i = 1; i <= n; i++) cin >> a[i];
 
-    vector<pii> tmp; tmp.push_back({ n, a[n] });
-    vector<int> ans = { -1 };
+    ans[n] = -1;
 
     for (int i = n - 1; i >= 1; i--)
     {
-        int idx = 1e9;
-
-        for (auto to : tmp)
-        {
-            if (to.second != a[i])
-                idx = min(idx, to.first);
-        }
-
-        if (idx == 1e9) idx = -1;
-        ans.push_back(idx);
-
-        tmp.push_back({ i, a[i] });
-        int sz = tmp.size();
-
-        bool flag = true;
-        
-        for (int x = 0; x < sz && flag; x++) for (int y = x + 1; y < sz && flag; y++)
-        {
-            if (tmp[x].second == tmp[y].second)
-            {
-                tmp.erase(tmp.begin() + x);
-                flag = false;
-                break;
-            }
-        }
-
-        if (tmp.size() == 3)
-            tmp.erase(tmp.begin());
+        if (a[i] == a[i + 1])
+            ans[i] = ans[i + 1];
+        else
+            ans[i] = i + 1;
     }
 
-    reverse(ans.begin(), ans.end());
-
-    for (auto to : ans)
-        cout << to << ' ';
+    for (int i = 1; i <= n; i++)
+        cout << ans[i] << ' ';
 }
