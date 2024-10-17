263f2475267a7c317eb7ad8d9c3baa04fc648da1
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-10-17 21:11:59 +0900

    [level 2] Title: 더 맵게, Time: 1494.66 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,39 +1,32 @@
 import java.util.*;
 
-class Value {
-    int val;
-    public Value(int val) {
-        this.val = val;
-    }
-}
-
 class Solution 
 {
-    boolean cmp(int v1, int v2) {
-        return v1 < v2;
+    int cmp(int v1, int v2)
+    {
+        if (v1 < v2) return -1;
+        else if (v1 == v2) return 0;
+        else return 1;
     }
     
-    PriorityQueue<Value> pq = new PriorityQueue<>(
-        (o1, o2) -> cmp(o1.val, o2.val) ? -1 : 1);
-    
-    public int solution(int[] sc, int K) 
+    public int solution(int[] a, int k) 
     {
+        int n = a.length;
         int ans = 0;
-        int n = sc.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (o1, o2) -> cmp(o1, o2));
         
         for (int i = 0; i < n; i++)
-            pq.add(new Value(sc[i]));
+            pq.add(a[i]);
         
-        while (pq.peek().val < K)
+        while (true)
         {
-            if (pq.size() < 2)
-                return -1;
+            if (pq.peek() >= k) return ans;
+            if (pq.size() <= 1) return -1;
             
-            int x = pq.poll().val, y = pq.poll().val;
-            pq.add(new Value(x + y * 2));
-            ans++;
+            int v1 = pq.peek(); pq.poll();
+            int v2 = pq.peek(); pq.poll();
+            ans++; pq.add(v1 + v2 * 2);
         }
-        
-        return ans;
     }
 }
