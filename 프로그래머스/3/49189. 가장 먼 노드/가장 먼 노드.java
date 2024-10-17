1f2dc8089f92aa5a461fc12205da7a7debb2572e
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-10-17 21:30:34 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.16 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,48 +2,53 @@ import java.util.*;
 
 class Solution 
 {
-    public static final int N = 20002;
+    static final int N = 20002;
     ArrayList<Integer>[] g = new ArrayList[N];
     int[] dist = new int[N];
-    int mx = -1, ans = 0;
     
     public int solution(int n, int[][] edge) 
     {
-        for (int i = 1; i <= n; i++) 
+        for (int i = 1; i <= n; i++)
         {
             dist[i] = -1;
             g[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i < edge.length; i++)
+        int ans = 0;
+        int len = edge.length;
+        for (int i = 0; i < len; i++)
         {
             int u = edge[i][0], v = edge[i][1];
-            g[u].add(v);
-            g[v].add(u);
+            g[u].add(v); g[v].add(u);
         }
         
-        dist[1] = 0;
-        Queue<Integer> q = new LinkedList<>(); q.add(1);
-
+        Queue<Integer> q = new LinkedList<>(); 
+        q.add(1); dist[1] = 0;
         while (!q.isEmpty())
         {
-            int v = q.poll();
+            int v = q.peek(); q.poll();
+            for (int nv : g[v])
+            {
+                if (dist[nv] == -1)
+                {
+                    dist[nv] = dist[v] + 1;
+                    q.add(nv);
+                }
+            }
+        }
+        
+        int mx = -1, cnt = 0;
+        for (int v = 1; v <= n; v++)
+        {
             if (mx < dist[v])
             {
                 mx = dist[v];
-                ans = 1;
+                cnt = 1;
             }
             else if (mx == dist[v])
-                ans++;
-            
-            for (int nv : g[v])
-            {
-                if (dist[nv] != -1) continue;
-                dist[nv] = dist[v] + 1;
-                q.add(nv);
-            }
+                cnt++;
         }
         
-        return ans;
+        return cnt;
     }
 }
