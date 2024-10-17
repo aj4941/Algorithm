93afedde96abcefcc3d77182b86dfbde988a71c0
Jeong Yong Choi <51875177+aj4941@users.noreply.github.com>
2024-10-17 21:44:24 +0900

    [level 2] Title: 배달, Time: 4.99 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 2.82 ms
+메모리: 70.6 MB, 시간: 4.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 13:07:30
+2024년 10월 17일 21:44:21
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Pair {
     int v, w;
-    
     public Pair(int v, int w) {
         this.v = v;
         this.w = w;
@@ -11,43 +10,47 @@ class Pair {
 
 class Solution 
 {
-    static final int N = 100;
-    ArrayList<Pair>[] g = new ArrayList[N];
-    int[] dist = new int[N];
+    static final int N = 102;
+    ArrayList<Pair> []g = new ArrayList[N];
+    int []dist = new int[N];
     
-    public int solution(int n, int[][] edge, int k) 
+    int cmp(Pair a, Pair b)
     {
-        for (int i = 0; i <= n; i++)
+        if (a.w < b.w) return -1;
+        else if (a.w == b.w) return 0;
+        else return 1;
+    }
+    
+    public int solution(int n, int[][] road, int k) 
+    {
+        for (int i = 1; i <= n; i++)
         {
+            dist[i] = -1;
             g[i] = new ArrayList<>();
-            dist[i] = (int)1e9;
         }
         
-        int sz = edge.length;
-        
-        for (int i = 0; i < sz; i++)
+        for (int i = 0; i < road.length; i++)
         {
-            int u = edge[i][0], v = edge[i][1], w = edge[i][2];
+            int u = road[i][0], v = road[i][1], w = road[i][2];
             g[u].add(new Pair(v, w));
             g[v].add(new Pair(u, w));
         }
         
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (o1, o2) -> (o1.w < o2.w) ? -1 : 1);
-        
-        dist[1] = 0; pq.add(new Pair(1, 0));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((o1, o2) -> cmp(o1, o2));
+        pq.add(new Pair(1, 0)); dist[1] = 0;
         
         while (!pq.isEmpty())
         {
-            int v = pq.peek().v, d = pq.peek().w; pq.poll();
-            if (dist[v] != d) continue;
-            for (Pair to : g[v])
+            int w = pq.peek().w, v = pq.peek().v; pq.poll();
+            if (dist[v] != w) continue;
+            
+            for (Pair p : g[v])
             {
-                int nv = to.v, w = to.w;
-                if (dist[v] + w < dist[nv])
+                int nv = p.v, nw = p.w;
+                if (dist[nv] == -1 || dist[v] + nw < dist[nv])
                 {
-                    dist[nv] = dist[v] + w;
+                    dist[nv] = dist[v] + nw;
                     pq.add(new Pair(nv, dist[nv]));
                 }
             }
